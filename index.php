f030b038a6761faea4cb059bf1742b2e26c48255
nibilly <nicolas.billy.etu.uca@gmail.com>
2020-04-27 22:37:32 +0200

    Comments

--- index.php
+++ index.php
@@ -1,16 +1,48 @@
 <?php
-# Get all events
+/*
+ * Requêtes
+@GET
+public Response getUser(@FormParam("firstName") String firstName, @FormParam("lastName") String lastName);
+
+@POST
+public Response postUser(@FormParam("firstName") String firstName, @FormParam("lastName") String lastName);
+
+@POST
+@Path("{userId}")
+public Response postAgenda(@PathParam("userId") int userId);
+
+@DELETE
+@Path("{userId}")
+public Response deleteAgenda(@PathParam("userId") int userId);
+
+@GET
+@Path("{userId}/event")
+public Response getEvents(@PathParam("userId") int userId);
+
+@POST
+@Path("{userId}/event")
+public Response postEvent(@PathParam("userId") int userId, @FormParam("startDate") String startDate,
+	@FormParam("finishDate") String finishDate, @FormParam("name") String name);
+
+@GET
+@Path("{userId}/event/day")
+public Response getEventByDay(@PathParam("userId") int userId, @QueryParam("date") String date);
+
+@DELETE
+@Path("{userId}/event/{eventId}")
+public Response deleteEvent(@PathParam("userId") int userId, @PathParam("eventId") int eventId);
+ */
 function getEvents()
 {
-    echo "01/12/2020 de 10H00 à 12H00 : Droit <br>01/12/2020 de 13H30 à 15H30 : Com";
-    /*$curl = curl_init();
+    # echo "01/12/2020 de 10H00 à 12H00 : Droit <br>01/12/2020 de 13H30 à 15H30 : Com";
+    $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, "http://localhost:8080/Agenda/api/event");
     $response = curl_exec($curl);
     $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
     $header_string = substr($response, 0, $header_size);
     $body = substr($response, $header_size);
     echo $body;
-    curl_close($curl);*/
+    curl_close($curl);
 }
 ?>
 <!DOCTYPE html>
