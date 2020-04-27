e0d8de28d8c2ea84747c82ece350cca736256082
nibilly <nicolas.billy.etu.uca@gmail.com>
2020-04-27 22:39:07 +0200

    Comments

--- index.php
+++ index.php
@@ -34,15 +34,15 @@ public Response deleteEvent(@PathParam("userId") int userId, @PathParam("eventId
  */
 function getEvents()
 {
-    # echo "01/12/2020 de 10H00 à 12H00 : Droit <br>01/12/2020 de 13H30 à 15H30 : Com";
-    $curl = curl_init();
+    echo "01/12/2020 de 10H00 à 12H00 : Droit <br>01/12/2020 de 13H30 à 15H30 : Com";
+    /*$curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, "http://localhost:8080/Agenda/api/event");
     $response = curl_exec($curl);
     $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
     $header_string = substr($response, 0, $header_size);
     $body = substr($response, $header_size);
     echo $body;
-    curl_close($curl);
+    curl_close($curl);*/
 }
 ?>
 <!DOCTYPE html>
